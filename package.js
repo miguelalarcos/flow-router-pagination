208dbb3db75c6fb6b7a199514e1945654463664a
miguelalarcos <miguel.alarcos@gmail.com>
2015-07-08 17:07:43 +0200

    package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'miguelalarcos:flow-router-pagination',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'A simple pagination package for flow-router.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/miguelalarcos/flow-router-pagination.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
